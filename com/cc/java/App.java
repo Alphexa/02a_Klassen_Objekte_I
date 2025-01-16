d7f000ac74c3c21ca6cfacd94aa7f96f0819e1fe
Alexander <alexander.pe.mobil@gmail.com>
2025-01-16 10:44:03 +0100

    1. Iteration Objekte durch new erzeugen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,9 @@ public class App {
     
     public static void main(String[] args) {
 
-
+        // 1. Iteration Objekte erzeugen durch "new" 
+        System.out.println(new Cat());
+        System.out.println(new Cat());
     }
 
    
