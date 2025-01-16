aadad2734c25d106b171d1f63d2bea036fe95d44
Alexander <alexander.pe.mobil@gmail.com>
2025-01-16 11:15:41 +0100

    2 Iteration / Instanziierung II Konstruktor

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,13 +5,20 @@ public class App {
     public static void main(String[] args) {
 
         // 2. Iteration
-        //Instanziierung ---> erzeugen eines Objektes
+        //Instanziierung ---> erzeugen eines Objektes duch 
+        // Aufruf eines Konstruktors
         // Objekt <----> Instanz
         // cat --> Referenzvariable
         // Cat --> Referenztyp (Klasse)
 
         Cat cat = new Cat();
         System.out.println(cat);
+        
+        System.out.println("----------------------");
+
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+
 
     }
 
